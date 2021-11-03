8a1ba693021edf4eeec8b4685539d964b9044a71
jarmeister <jarmeister99@gmail.com>
2021-11-03 14:11:20 -0700

    edit view working

--- src/views/home_page/posts/Post.js
+++ src/views/home_page/posts/Post.js
@@ -1,14 +1,21 @@
 import axios from "axios";
 import dateformat from 'dateformat'
-import { Button, Accordion } from 'react-bootstrap';
+import { Button, Accordion, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 const Post = (props) => {
   const postData = props.post;
   const posts = props.posts;
   const setPosts = props.setPosts;
   const user = props.user;
+
+  const [editFlag, setEditFlag] = useState(false);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('')
+
   const formattedDate = dateformat(postData.createdOn, "mmmm dS, yyyy")
-  const deleteHandler = () => {
+  const deleteHandler = (e) => {
+    e.preventDefault();
     const postId = postData._id;
     axios.delete(`${process.env.REACT_APP_API_URL}/posts`, { data: { _id: postId } }).then((response) => {
       if (response.data.deletedCount > 0) {
@@ -18,6 +25,12 @@ const Post = (props) => {
     }).catch(error => {
       console.log(error);
     })
+  }
+  const editHandler = (e) => {
+    e.preventDefault();
+    setTitle(postData.title);
+    setContent(postData.content);
+    setEditFlag(!editFlag);
   }
   const containerStyle = {
     marginBottom: "10px",
@@ -39,27 +52,63 @@ const Post = (props) => {
     overflowWrap: "break-word"
   }
 
-  return (
-    <div style={containerStyle}>
-
-      <Accordion>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>
+  if (!editFlag) {
+    return (
+      <div style={containerStyle}>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
+              <div style={{ width: "100%" }}>
+                <span style={titleStyle}>{postData.title}</span><br /><span style={titleInfoStyle}>Created {formattedDate} by {postData.user}</span>
+              </div>
+            </Accordion.Header>
+            <Accordion.Body>
+              <p style={contentStyle}>{postData.content}</p>
+              {user === postData.user &&
+                <span>
+                  <Button size="sm" variant="outline-danger" style={{ marginRight: "5px", marginBottom: "5px" }} onClick={deleteHandler}>Delete</Button>
+                  <Button size="sm" variant="outline-dark" style={{ marginRight: "5px", marginBottom: "5px" }} onClick={editHandler}>Edit</Button>
+                </span>
+              }
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div style={containerStyle}>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
             <div style={{ width: "100%" }}>
-              <span style={titleStyle}>{postData.title}</span><br/><span style={titleInfoStyle}>Created {formattedDate} by {postData.user}</span>
-
-            </div>
-          </Accordion.Header>
-          <Accordion.Body>
-            <p style={contentStyle}>{postData.content}</p>
-            {user === postData.user &&
-              <span><Button size="sm" variant="outline-danger" style={{ marginRight: "5px", marginBottom: "5px" }} onClick={deleteHandler}>Delete</Button></span>
-            }
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-    </div>
-  )
+                <span style={titleStyle}>{postData.title}</span><br /><span style={titleInfoStyle}>Created {formattedDate} by {postData.user}</span>
+              </div>
+            </Accordion.Header>
+            <Accordion.Body>
+              <Form>
+                <Form.Group className="mb-3">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control type="text" placeholder="Creative title" value={title} onChange={e => setTitle(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Content</Form.Label>
+                  <Form.Control as="textarea" rows={5} value={content} onChange={e => setContent(e.target.value)} />
+                </Form.Group>
+                <Button className="me-3" variant="primary" type="button">
+                  Submit
+                </Button>
+                <Button className="btn-sm" variant="outline-danger" type="button" onClick={editHandler}>
+                  Cancel
+                </Button>
+              </Form>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
+    )
+  }
 }
 
 export default Post;
